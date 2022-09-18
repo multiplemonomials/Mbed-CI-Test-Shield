ac644ed4ce3e3b345b483c587329805b550a9258
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2022-09-18 10:29:04 -0700

    Update project for mbed-ce changes, finish SPI test

--- Software/SPITest.cpp
+++ Software/SPITest.cpp
@@ -87,78 +87,85 @@ void test_card_present()
     destroySDBlockDev(sdDev);
 }
 
-//// Test for SD Card being present on the shield
-//void test_card_present()
-//{
-//    int error = 0;
-//    SDBlockDevice sd(MBED_CONF_APP_SPI_MOSI, MBED_CONF_APP_SPI_MISO, MBED_CONF_APP_SPI_CLK, MBED_CONF_APP_SPI_CS);
-//    FATFileSystem fs("sd");
-//    sd.init();
-//
-//    error = fs.mount(&sd);
-//    if(error)
-//        error = fs.reformat(&sd);
-//
-//    TEST_ASSERT_MESSAGE(error==0,"SD file system mount failed.");
-//
-//    FILE *File = fopen("/sd/card-present.txt", "w+");
-//
-//    TEST_ASSERT_MESSAGE(File != NULL,"SD Card is not present. Please insert an SD Card.");
-//
-//    error = fs.unmount();
-//    TEST_ASSERT_MESSAGE(error==0,"SD file system unmount failed.");
-//
-//    sd.deinit();
-//}
-//
-//// Test SD Card Write
-//void test_sd_w()
-//{
-//    int error = 0;
-//    SDBlockDevice sd(MBED_CONF_APP_SPI_MOSI, MBED_CONF_APP_SPI_MISO, MBED_CONF_APP_SPI_CLK, MBED_CONF_APP_SPI_CS);
-//    FATFileSystem fs("sd", &sd);
-//
-//    sd.init();
-//
-//    error = fs.mount(&sd);
-//
-//    FILE *File = fopen("/sd/test_sd_w.txt", "w");
-//    TEST_ASSERT_MESSAGE(File != NULL,"SD Card is not present. Please insert an SD Card.");
-//    TEST_ASSERT_MESSAGE(fprintf(File, SD_TEST_STRING) > 0,"Writing file to sd card failed");             // write data
-//    // TODO: test that the file was written correctly
-//    fclose(File);                              // close file on SD
-//    TEST_ASSERT(true);
-//
-//    error = fs.unmount();
-//
-//    sd.deinit();
-//}
-//
-//// Test SD Card Read
-//void test_sd_r()
-//{
-//    char read_string [SD_TEST_STRING_MAX] = {0};
-//    int error = 0;
-//    SDBlockDevice sd(MBED_CONF_APP_SPI_MOSI, MBED_CONF_APP_SPI_MISO, MBED_CONF_APP_SPI_CLK, MBED_CONF_APP_SPI_CS);//alvin add
-//    FATFileSystem fs("sd", &sd);
-//
-//    sd.init();
-//
-//    error = fs.mount(&sd);
-//
-//    FILE *File = fopen("/sd/test_sd_w.txt", "r");
-//    TEST_ASSERT_MESSAGE(File != NULL,"SD Card is not present. Please insert an SD Card.");
-//    fgets(read_string,SD_TEST_STRING_MAX,File); // read string from the file
-//    //fread(read_string,sizeof(char),sizeof(SD_TEST_STRING),File); // read the string and compare
-//    DEBUG_PRINTF("\r\n****\r\nRead '%s' in read test\r\n, string comparison returns %d\r\n****\r\n",read_string,strcmp(read_string,SD_TEST_STRING));
-//    TEST_ASSERT_MESSAGE(strcmp(read_string,SD_TEST_STRING) == 0,"String read does not match string written"); // test that strings match
-//    fclose(File);    // close file on SD
-//    TEST_ASSERT(true);
-//
-//    error = fs.unmount();
-//
-//    sd.deinit();
-//}
+// Test which mounts the filesystem and creates a file
+template<bool use_gpio_cs, uint64_t spiFreq>
+void mount_fs_create_file()
+{
+    SDBlockDevice * sdDev = constructSDBlockDev(use_gpio_cs, spiFreq);
+    FATFileSystem fs("sd");
+
+	int ret = sdDev->init();
+    TEST_ASSERT_MESSAGE(ret == BD_ERROR_OK, "Failed to connect to SD card");
+
+    ret = fs.mount(sdDev);
+
+    if(ret)
+	{
+		// This is expected if the SD card was not formatted previously
+		ret = fs.reformat(sdDev);
+	}
+
+    TEST_ASSERT_MESSAGE(ret==0,"SD file system mount failed.");
+
+    FILE * file = fopen("/sd/card-present.txt", "w+");
+
+    TEST_ASSERT_MESSAGE(file != nullptr,"Failed to create file");
+
+	fclose(file);
+
+    ret = fs.unmount();
+    TEST_ASSERT_MESSAGE(ret==0,"SD file system unmount failed.");
+
+    destroySDBlockDev(sdDev);
+}
+
+// Test which writes, reads, and deletes a file.
+template<bool use_gpio_cs, uint64_t spiFreq>
+void test_sd_file()
+{
+    SDBlockDevice * sdDev = constructSDBlockDev(use_gpio_cs, spiFreq);
+    FATFileSystem fs("sd");
+
+	int ret = sdDev->init();
+    TEST_ASSERT_MESSAGE(ret == BD_ERROR_OK, "Failed to connect to SD card");
+
+    ret = fs.mount(sdDev);
+	TEST_ASSERT_MESSAGE(ret==0,"SD file system mount failed.");
+
+	// Write the test string to a file.
+    FILE * file = fopen("/sd/test_sd_w.txt", "w");
+    TEST_ASSERT_MESSAGE(file != nullptr,"Failed to create file");
+	init_string();
+    TEST_ASSERT_MESSAGE(fprintf(file, SD_TEST_STRING) > 0,"Writing file to sd card failed");
+    fclose(file);
+
+	// Now open it and read the string back
+	char read_string[SD_TEST_STRING_MAX] = {0};
+    file = fopen("/sd/test_sd_w.txt", "r");
+	TEST_ASSERT_MESSAGE(file != nullptr,"Failed to open file");
+
+	ret = fread(read_string, sizeof(char), sizeof(SD_TEST_STRING), file);
+	TEST_ASSERT_MESSAGE(ret == SD_TEST_STRING_MAX, "Failed to read data");
+	DEBUG_PRINTF("\r\n****\r\nRead '%s' in read test\r\n, string comparison returns %d\r\n****\r\n",read_string,strcmp(read_string,SD_TEST_STRING));
+	TEST_ASSERT_MESSAGE(strcmp(read_string,SD_TEST_STRING) == 0,"String read does not match string written");
+
+	// Check that reading one additional char causes an EOF error
+	ret = fread(read_string, sizeof(char), 1, file);
+	TEST_ASSERT_MESSAGE(ret < 1, "fread did not return error?");
+	TEST_ASSERT(feof(file));
+
+	fclose(file);
+
+	// Delete the file and make sure it's gone
+	remove("/sd/test_sd_w.txt");
+	TEST_ASSERT(fopen("/sd/test_sd_w.txt", "w") == nullptr);
+
+	// Clean up
+	ret = fs.unmount();
+    TEST_ASSERT_MESSAGE(ret==0,"SD file system unmount failed.");
+
+    destroySDBlockDev(sdDev);
+}
 
 utest::v1::status_t test_setup(const size_t number_of_cases)
 {
@@ -173,22 +180,24 @@ Case cases[] = {
         Case("SPI - Object Definable (HW CS)", test_object<false>),
         Case("SPI - SD card present (1MHz, HW CS)", test_card_present<false, 1000000>),
         Case("SPI - SD card present (10MHz, HW CS)", test_card_present<false, 10000000>),
+		Case("SPI - Mount FS, Create File (1MHz, HW CS)", mount_fs_create_file<false, 1000000>),
+		Case("SPI - Mount FS, Create File (10MHz, HW CS)", mount_fs_create_file<false, 10000000>),
+		Case("SPI - Write, Read, and Delete File (1MHz, HW CS)", test_sd_file<false, 1000000>),
+		Case("SPI - Write, Read, and Delete File (10MHz, HW CS))", test_sd_file<false, 10000000>),
 #endif
 
         Case("SPI - Object Definable (GPIO CS)", test_object<true>),
         Case("SPI - SD card present (1MHz, GPIO CS)", test_card_present<true, 1000000>),
         Case("SPI - SD card present (10MHz, GPIO CS)", test_card_present<true, 10000000>),
-
-//        Case("SPI - SD Write",     test_sd_w,greentea_failure_handler),
-//        Case("SPI - SD Read",     test_sd_r,greentea_failure_handler),
-//        Case("SPI - SD 2nd Write",     test_sd_w,greentea_failure_handler),
-//        Case("SPI - SD 2nd Read",     test_sd_r,greentea_failure_handler),
+		Case("SPI - Mount FS, Create File (1MHz, GPIO CS)", mount_fs_create_file<true, 1000000>),
+		Case("SPI - Mount FS, Create File (10MHz, GPIO CS)", mount_fs_create_file<true, 10000000>),
+		Case("SPI - Write, Read, and Delete File (1MHz, GPIO CS)", test_sd_file<true, 1000000>),
+		Case("SPI - Write, Read, and Delete File (10MHz, GPIO CS))", test_sd_file<true, 10000000>),
 };
 
 Specification specification(test_setup, cases, greentea_continue_handlers);
 
 // // Entry point into the tests
 int main() {
-    init_string();
     return !Harness::run(specification);
 }
